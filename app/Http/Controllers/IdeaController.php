c4949cca47750c8d96d688ada23cf976782f3d73
bhagat-abhishek <inst.abhishek@gmail.com>
2024-01-17 00:54:53 +0530

    validation

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -10,7 +10,7 @@ class IdeaController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'content' => 'required|string|max:150'
+            'content' => 'required|string|min:3|max:150'
         ]);
 
         Idea::create([
--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -4,6 +4,9 @@
         @csrf
         <div class="mb-3">
             <textarea name="content" class="form-control" id="idea" rows="3"></textarea>
+            @error('content')
+                <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+            @enderror
         </div>
         <div class="">
             <button class="btn btn-dark" type="submit"> Share </button>
