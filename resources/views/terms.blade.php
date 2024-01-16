a6ab115f67fc3401ef92f3b65d668267124afa9a
bhagat-abhishek <inst.abhishek@gmail.com>
2024-01-16 22:22:18 +0530

    template layout setup

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum atque rerum necessitatibus! Debitis neque,
+        reprehenderit nihil ducimus ex asperiores consequatur molestias? Ab at iure ad dicta dolore doloribus
+        molestiae aperiam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum atque rerum necessitatibus! Debitis
+        neque,
+        reprehenderit nihil ducimus ex asperiores consequatur molestias? Ab at iure ad dicta dolore doloribus
+        molestiae aperiam.
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
-Route::get('/profile', [ProfileController::class, 'profile']);
+
+Route::view('/terms', 'terms');
